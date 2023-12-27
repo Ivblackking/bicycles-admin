dad74297ad863a7e9525048428eccf80016971d1
Ivblackking <ivan.lysenko305@gmail.com>
2023-12-28 00:01:58 +0200

    added handlers for inputs fields

--- client/src/Components/BicycleForm/BicycleForm.js
+++ client/src/Components/BicycleForm/BicycleForm.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import "./BicycleForm.css";
+import {useState} from 'react';
 
 function BicycleForm() {
+    const [name, setName] = useState("");
+    const [type, setType] = useState("");
+    const [color, setColor] = useState("");
+    const [wheelSize, setWheelSize] = useState(0.0);
+    const [price, setPrice] = useState(0.0);
+    const [visibleId, setVisibleId] = useState("");
+    const [description, setDescription] = useState("");
+
     return (
         <form>
             <div className='inputs-text'>
-                <input type="text" placeholder='Name'/>
-                <input type="text" placeholder='Type'/>
-                <input type="text" placeholder='Color'/>
-                <input type="text" placeholder='Wheel size'/>
-                <input type="text" placeholder='Price'/>
-                <input type="text" placeholder='ID (slug): XXXXXXXXXXXXX'/>
+                <input onChange={e=>setName(e.target.value)} type="text" placeholder='Name'/>
+                <input onChange={e=>setType(e.target.value)} type="text" placeholder='Type'/>
+                <input onChange={e=>setColor(e.target.value)} type="text" placeholder='Color'/>
+                <input onChange={e=>setWheelSize(Number(e.target.value))} type="text" placeholder='Wheel size'/>
+                <input onChange={e=>setPrice(Number(e.target.value))} type="text" placeholder='Price'/>
+                <input onChange={e=>setVisibleId(e.target.value)} type="text" placeholder='ID (slug): XXXXXXXXXXXXX'/>
             </div>
-            <textarea placeholder="Description" className='description'></textarea>
+            <textarea onChange={e=>setDescription(e.target.value)} placeholder="Description" className='description'/>
             <div className='buttons'>
                 <button>SAVE</button>
                 <button>CLEAR</button>
