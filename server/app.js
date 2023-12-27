87383d4971384b5109d6a0d1b38dc4b3db290a94
Ivblackking <ivan.lysenko305@gmail.com>
2023-12-27 11:25:41 +0200

    added post and patch empty functions

--- server/app.js
+++ server/app.js
@@ -6,4 +6,7 @@ app.get("/api", (req, res) => {
     res.send({ message: "Hello from Express!" });
 });
 
+app.post("/api", (req, res)=>{});
+app.patch("/api", (req, res)=>{});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
