132f863210940c700f6deba1d548560ad7ef5e8f
Ivblackking <ivan.lysenko305@gmail.com>
2023-12-27 22:32:05 +0200

    added form

--- client/src/Components/BicycleForm/BicycleForm.js
+++ client/src/Components/BicycleForm/BicycleForm.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./BicycleForm.css";
 
 function BicycleForm() {
-  return (
-    <div>BicycleForm</div>
+    return (
+        <form>
+            <div className='inputs-text'>
+                <input type="text" placeholder='Name'/>
+                <input type="text" placeholder='Type'/>
+                <input type="text" placeholder='Color'/>
+                <input type="text" placeholder='Wheel size'/>
+                <input type="text" placeholder='Price'/>
+                <input type="text" placeholder='ID (slug): XXXXXXXXXXXXX'/>
+            </div>
+            <textarea placeholder="Description" className='description'></textarea>
+            <div className='buttons'>
+                <button>SAVE</button>
+                <button>CLEAR</button>
+            </div>
+        </form>
   )
 }
 
