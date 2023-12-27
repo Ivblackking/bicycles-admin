edd85ee590715aacb7b81f19e3967e66750fac5c
Ivblackking <ivan.lysenko305@gmail.com>
2023-12-27 12:01:16 +0200

    connection to mongo db is OK

--- server/app.js
+++ server/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 3500;
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('connected', () => {
+    console.log('Database Connected');
+});
 
 app.get("/api", (req, res) => {
     res.send({ message: "Hello from Express!" });
