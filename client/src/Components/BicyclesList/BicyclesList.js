b3180984566ff65a732b7a19803c15056ebee639
Ivblackking <ivan.lysenko305@gmail.com>
2023-12-28 12:22:19 +0200

    axios get has been moved in componet Main

--- client/src/Components/BicyclesList/BicyclesList.js
+++ client/src/Components/BicyclesList/BicyclesList.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import BicycleItem from '../BicycleItem/BicycleItem';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
 
-function BicyclesList() {
-    const [bicycles, setBicyles] = useState([]);
-  
-    useEffect(() => {
-      const getDataFromAPI = async () => {
-        try {
-            const response = await axios.get('api/v1/bicycles');
-            const data = await response.data;
-            setBicyles(data);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      getDataFromAPI();
-    }, [bicycles]);
-
+function BicyclesList({bicycles}) {
     return (
         <ul className='ps-0'>
             {bicycles.map(bicycle =>
--- client/src/Components/Main/Main.js
+++ client/src/Components/Main/Main.js
@@ -2,19 +2,36 @@ import React from 'react'
 import BicyclesList from '../BicyclesList/BicyclesList'
 import BicycleForm from '../BicycleForm/BicycleForm'
 import Statistics from '../Statistics/Statistics'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Main.css';
 
 function Main() {
+  const [bicycles, setBicyles] = useState([]);
+  
+    useEffect(() => {
+      const getDataFromAPI = async () => {
+        try {
+            const response = await axios.get('api/v1/bicycles');
+            const data = await response.data;
+            setBicyles(data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      getDataFromAPI();
+    }, [bicycles]);
+
   return (
     <div className='container-fluid'>
         <div className='row'>
             <div className='mycol col-7'>
-                <BicyclesList/>
+                <BicyclesList bicycles={bicycles}/>
             </div>
             <div className='mycol col-5'>
                 <BicycleForm/>
-                <Statistics/>
+                <Statistics bicycles={bicycles}/>
             </div>
         </div>
     </div>
