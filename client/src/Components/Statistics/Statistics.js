858061a2d709379b83883a7f522a1d8b851b4288
Ivblackking <ivan.lysenko305@gmail.com>
2023-12-28 15:14:50 +0200

    all buttons and componets wors

--- client/src/Components/Statistics/Statistics.js
+++ client/src/Components/Statistics/Statistics.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function Statistics(props) {
+function Statistics({bicycles}) {
+  const totalBikes = bicycles.length;
+  const availableBikes = bicycles.filter(el=>el.status === "Available").length;
+  const bookedBikes = bicycles.filter(el=>el.status === "Busy").length;
+  const averagBikeCost = bicycles.map(el=>el.price).reduce((a,b)=>a+b, 0) / totalBikes;
+
   return (
-    <div>Statistics</div>
+    <div>
+      <h5>Statistics</h5>
+      <p>
+        Total Bikes: <span>{totalBikes}</span><br></br>
+        Available Bikes : <span>{availableBikes}</span><br></br>
+        Booked Bikes: <span>{bookedBikes}</span><br></br>
+        Average bike cost: <span>{averagBikeCost.toFixed(2)}</span> UAH/hr.
+      </p>
+    </div>
   )
 }
 
